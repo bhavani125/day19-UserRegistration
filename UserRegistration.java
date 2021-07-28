d76cbdb9f1aca35295cba0c251f770159a8bd73e
bhavani125 <girinenibhavani12@gmil.com>
2021-07-28 19:25:52 +0530

    Added: Code of validating FirstName

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,34 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to user registration program");
-    }
+
+        Scanner sc = new Scanner(System.in);
+        public static final String namePattern = "[A-Z]{1}[A-Za-z]{2,}";
+        //Main method
+        public static void main(String[] args)
+        {
+            //Creating object for userRegistration class
+            UserRegistration userRegistration = new UserRegistration();
+            //calling readInput method here
+            userRegistration.readInput();
+        }
+        //Creating readInput method to take input from user
+        private void readInput()
+        {
+            System.out.println("Enter first name ");
+            String firstName = sc.next();
+            validateFirstName(namePattern,firstName);
+        }
+        //Creating validateFirstName method to check
+        private void validateFirstName(String pattern, String input) {
+
+            if (Pattern.matches(pattern, input)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
 }
